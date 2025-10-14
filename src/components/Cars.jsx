ae501a8292cf09cac7b56c664ded3bcb931bf7c0
Cintya Rios <cintya.christovam@germinare.org.br>
2025-10-14 20:39:02 -0300

    style: estilizacao dos cards

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -1,24 +1,36 @@
-import styles from "./Cars.module.css"; // 👈 importa o CSS module
+import styles from "./Cars.module.css";
+
+import ferrari from "../assets/ferrari488gtb.jpg";
+import tesla from "../assets/Tesla.jpg";
+import bmw from "../assets/bmw.jpg";
+import lamborghini from "../assets/lamborghini.jpg";
+import porsche from "../assets/porsche.jpeg";
+import audi from "../assets/audir8.jpeg";
 
 const Cars = () => {
   const carros = [
-    { id: 1, marca: "Ferrari", modelo: "488 GTB", ano: 2020, cor: "Vermelho" },
-    { id: 2, marca: "Tesla", modelo: "Model S", ano: 2022, cor: "Branco" },
-    { id: 3, marca: "BMW", modelo: "M4 Competition", ano: 2021, cor: "Azul" },
-    { id: 4, marca: "Lamborghini", modelo: "Huracán EVO", ano: 2019, cor: "Amarelo" },
-    { id: 5, marca: "Porsche", modelo: "911 Carrera", ano: 2023, cor: "Cinza" },
-    { id: 6, marca: "Audi", modelo: "R8", ano: 2020, cor: "Preto" },
+    { id: 1, foto: ferrari, marca: "Ferrari", modelo: "488 GTB", ano: 2020, cor: "Vermelho" },
+    { id: 2, foto: tesla, marca: "Tesla", modelo: "Model S", ano: 2022, cor: "Branco" },
+    { id: 3, foto: bmw, marca: "BMW", modelo: "M4 Competition", ano: 2021, cor: "Azul" },
+    { id: 4, foto: lamborghini, marca: "Lamborghini", modelo: "Huracán EVO", ano: 2019, cor: "Amarelo" },
+    { id: 5, foto: porsche, marca: "Porsche", modelo: "911 Carrera", ano: 2023, cor: "Cinza" },
+    { id: 6, foto: audi, marca: "Audi", modelo: "R8", ano: 2020, cor: "Preto" },
   ];
 
   return (
     <div className={styles.container}>
       <div className={styles.lista}>
         {carros.map((carro) => (
-          <div key={carro.id} className={styles.card}>
-            <h3>{carro.modelo}</h3>
-            <p><strong>Marca:</strong> {carro.marca}</p>
-            <p><strong>Ano:</strong> {carro.ano}</p>
-            <p><strong>Cor:</strong> {carro.cor}</p>
+          <div key={carro.id} className={styles.cardCarro}>
+            <img src={carro.foto} className={styles.foto} />
+            <div className={styles.card}>
+              <span className={styles.marca}>{carro.marca}</span>
+              <h3>{carro.modelo}</h3>
+              <div className={styles.detalhe}>
+                <p><strong>Ano:</strong> {carro.ano}</p>
+                <p><strong>Cor:</strong> {carro.cor}</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
