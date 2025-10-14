8d6f762a456c7097d19b7c0fbfaf3bad0cb8d556
Cintya Rios <cintya.christovam@germinare.org.br>
2025-10-14 07:41:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import MyComponent from "./components/MyComponent";
+import Title from "./components/Title"; // LINHA ADICIONADA
+
+function App() {
+  const n = 15;
+  const redTitle = true;
+
+  return (
+    <div className="App">
+      <h1>React com CSS</h1>
+      <p>Este parágrafo pegou estilo do componente!</p>
+      <MyComponent />
+      <p
+        style={{
+          color: "blue",
+          padding: "25px",
+          borderTop: "1px dotted blue",
+        }}
+      >
+        Este elemento foi estilizado inline
+      </p>
+      <h2 style={n > 10 ? { color: "purple" } : { color: "magenta" }}>
+        CSS dinâmico
+      </h2>
+      <h2 style={n < 10 ? { color: "purple" } : { color: "magenta" }}>
+        CSS dinâmico 2
+      </h2>
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Este título vai ter uma classe dinâmica
+      </h2>
+      <Title />
+      <h1 className="title">Este não recebe título do CSS modules</h1>
+    </div>
+  );
+}
+
+export default App;
